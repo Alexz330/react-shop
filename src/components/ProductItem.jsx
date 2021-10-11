4b08096ae671fd264dfe23bc300d383e24f6a6b9
Alexis Lopez <alexislc330@gmail.com>
2021-10-11 16:14:51 -0600

    first commit

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,32 @@
+import React,{useContext} from 'react';
+import appContext from "../context/AppContext"
+import '../styles/ProductItem.scss';
+import IconAddToCart from "@icons/bt_add_to_cart.svg"
+
+const ProductItem = ({product}) => {
+	const {addToCart} =  useContext(appContext);
+
+	
+	const handleClick =(item) =>{
+		addToCart(item)
+		console.log(item)
+	}
+
+	return (
+		<div className="ProductItem">
+			<img src={product.images[0]} alt={product.title} />
+			<div className="product-info">
+				<div>
+					<p>$ {product.price}</p>
+					<p>{product.title}</p>
+				</div>
+				<figure onClick={()=>handleClick(product)} >
+					<img src={IconAddToCart} />
+				</figure>
+			
+			</div>
+		</div>
+	);
+}
+
+export default ProductItem;
